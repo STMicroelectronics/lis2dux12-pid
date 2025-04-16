3d8cdee0fde50fdde9c6d72e6a9a742d05b67552
Armando Visconti <armando.visconti@st.com>
2025-04-16 15:38:36 +0200

    Fix fifo_mode_set
    Configure xl FIFO batching rate (bdr_xl) even if timestamp
    batching is set to off.
    
    Signed-off-by: Armando Visconti <armando.visconti@st.com>

--- lis2dux12_reg.c
+++ lis2dux12_reg.c
@@ -2004,11 +2004,8 @@ int32_t lis2dux12_fifo_mode_set(const stmdev_ctx_t *ctx, lis2dux12_fifo_mode_t v
     fifo_wtm.xl_only_fifo = val.xl_only;
 
     /* set batching info */
-    if (val.batch.dec_ts != LIS2DUX12_DEC_TS_OFF)
-    {
-      fifo_batch.dec_ts_batch = (uint8_t)val.batch.dec_ts;
-      fifo_batch.bdr_xl = (uint8_t)val.batch.bdr_xl;
-    }
+    fifo_batch.dec_ts_batch = (uint8_t)val.batch.dec_ts;
+    fifo_batch.bdr_xl = (uint8_t)val.batch.bdr_xl;
 
     fifo_ctrl.cfg_chg_en = val.cfg_change_in_fifo;
 
